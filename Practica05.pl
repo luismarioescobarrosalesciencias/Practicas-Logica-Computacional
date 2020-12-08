597c12db776eac70b59ff4ee8130ee0368daeeb3
MiguelAngelLiera <317257421@ciencias.unam.mx>
2020-12-08 11:24:34 -0600

    Practica 5 con los primeros 4 ejercicios

---
+++ Practica05.pl
@@ -0,0 +1,35 @@
+% Practica 5
+
+% 1. elemento. Función que determina si un elemento pertenece a una lista
+elemento(X,[X|_]).
+elemento(X,[_|XS]) :- elemento(X,XS).
+
+% 2. suma. Función que suma todos los elementos de una lista
+suma([], 0).
+suma([X|XS], S) :- suma(XS, R), S is X + R.
+
+% 3. palindromo. Funcion que verifica si una lista es palindromo o no.
+palindromo([]).
+palindromo([_]).
+palindromo(L) :- reversa(L,L).
+
+% reversa. Devuelve la reversa de una lista
+reversa([],[]).
+reversa([X|XS], Reversa):- reversa(XS, ReversaXS), conc(ReversaXS, X, Reversa).
+
+% conc. Concatena dos listas.
+conc([],X,[X]).
+conc([X|D],B,[X|E]) :- conc(D,B,E).
+
+% comparacion. Compara dos listas y determina si son iguales o no.
+%comparacion([],[]).
+%comparacion([X],[Y]) :- X =:= Y.
+%comparacion([X|XS],[Y|YS]) :- X =:= Y, comparacion(XS,YS).
+
+% 4. ocurrir. Funcion que devuelve el numero de apariciones de un elemento en
+%             una lista.
+ocurrir([],_,0).
+ocurrir([X],X,1).
+ocurrir([Y],X,0).
+ocurrir([X|XS],X,NumVeces) :- ocurrir(XS,X,S), NumVeces is 1 + S.
+ocurrir([_|YS],X,NumVeces) :- ocurrir(YS,X,S), NumVeces is S.
