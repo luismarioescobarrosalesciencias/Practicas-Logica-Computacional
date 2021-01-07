8164de908ee3be5769e28f03e88bfeaa822a1f32
MiguelAngelLiera <317257421@ciencias.unam.mx>
2021-01-07 01:14:07 -0600

    primeros dos ejercicios medio empezados

---
+++ Practica06.pl
@@ -0,0 +1,33 @@
+% Practica 5
+
+traducir(X,Y) :- dic(X,Y).
+traducir([X],[Y]) :- dic(X,Y).
+traducir([X|XS],[Y|YS]) :- dic(X,Y), traducir(XS,YS).
+
+% conc. Concatena dos listas.
+conc([],X,[X]).
+conc([X|D],B,[X|E]) :- conc(D,B,E).
+
+dic(tree, arbol).
+dic(cup, taza).
+
+matematico(pepe).
+sabemate(X) :- matematico(X).
+
+sobre(nada,amarillo).
+sobre(amarillo,rosa).
+sobre(rosa,nada).
+sobre(nada,verde).
+sobre(verde,nada).
+sobre(nada,gris).
+sobre(gris,rojo).
+sobre(rojo,azul).
+sobre(azul,nada).
+
+hastaArriba(X) :- sobre(nada,X).
+
+bloqueado(X) :- not(hastaArriba(X)).
+
+hastaAbajo(X) :- sobre(X,nada).
+
+mover(X,Y) :- not(bloqueado(X)), hastaArriba(Y).
