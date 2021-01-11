34c43dec91c0a803dc21f480564f1a02d350b944
Luis Mario <luismescobar@ciencias.unam.mx>
2021-01-10 19:21:08 -0600

    Parte 1 completa

--- Practica06.pl
+++ Practica06.pl
@@ -1,16 +1,60 @@
-% Practica 5
+% Practica 6
 
-traducir(X,Y) :- dic(X,Y).
-traducir([X],[Y]) :- dic(X,Y).
-traducir([X|XS],[Y|YS]) :- dic(X,Y), traducir(XS,YS).
 
-% conc. Concatena dos listas.
+
+% concatena listas 
 conc([],X,[X]).
 conc([X|D],B,[X|E]) :- conc(D,B,E).
 
+% traducir 
+% entrada: 
+% [hola,amigo,como estas]
+% saldida:
+% [hello,friend,how,are,you]
+
+dic(hello,hola).
+dic(a,un).
+dic(the,el).
+dic(of,de).
+dic(that,que).
+dic(in,en).
+dic(on,sobre).
+dic(by,por).
+dic(no,no).
+dic(but,pero).
+dic(those,aquellos).
+dic(who,quien).
+dic(can,puede).
+dic(cannot,no_puede).
+dic(is,es).
 dic(tree, arbol).
 dic(cup, taza).
 dic(maestro, teacher).
+dic(we_are,somos).
+dic(logic,logica).
+dic(team,equipo).
+dic(bird,pajaro).
+dic(student,estudiante).
+dic(students,estudiantes).
+dic(science,ciencias).
+dic(dynamite,dinamita).
+
+% la traduccion de una palabra es simetrica,
+%  no  estamos considerado sinonimos 
+traducir(X,Y) :- dic(X,Y).
+traducir(X,Y):-dic(Y,X).
+traducir([X],[Y]) :- dic(X,Y).
+traducir([X],[Y]) :- dic(Y,X).
+traducir([X|XS],[Y|YS]) :- dic(X,Y), traducir(XS,YS).
+traducir([X|XS],[Y|YS]) :- dic(Y,X), traducir(XS,YS).
+
+% Probar con la siguiente frase (español-ingles).
+% hola , somos, el equipo dinamita de logica, somos estudiantes de ciencias
+% Poner en terminal:   traducir([hola,somos,el,equipo,dinamita,de,logica,somos,estudiantes,de,ciencias],Y).
+
+% Probar ingles- español: traducir([hello,we_are,the,team,dynamite,of,logic,we_are,students,of,science],Y).
+
+
 
 matematico(pepe).
 sabemate(X) :- matematico(X).
