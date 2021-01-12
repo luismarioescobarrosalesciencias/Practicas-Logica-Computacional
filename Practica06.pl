9e43db6bf15b453ede73900c940a1b49367c7bb3
MiguelAngelLiera <317257421@ciencias.unam.mx>
2021-01-11 23:23:22 -0600

    cambios notables al segundo ejercicio

--- Practica06.pl
+++ Practica06.pl
@@ -2,12 +2,12 @@
 
 
 
-% concatena listas 
+% concatena listas
 conc([],X,[X]).
 conc([X|D],B,[X|E]) :- conc(D,B,E).
 
-% traducir 
-% entrada: 
+% traducir
+% entrada:
 % [hola,amigo,como estas]
 % saldida:
 % [hello,friend,how,are,you]
@@ -40,7 +40,7 @@ dic(science,ciencias).
 dic(dynamite,dinamita).
 
 % la traduccion de una palabra es simetrica,
-%  no  estamos considerado sinonimos 
+%  no  estamos considerado sinonimos
 traducir(X,Y) :- dic(X,Y).
 traducir(X,Y):-dic(Y,X).
 traducir([X],[Y]) :- dic(X,Y).
@@ -69,13 +69,52 @@ sobre(gris,rojo).
 sobre(rojo,azul).
 sobre(azul,nada).
 
+
 hastaArriba(X) :- sobre(nada,X).
 
 bloqueado(X) :- not(hastaArriba(X)).
 
 hastaAbajo(X) :- sobre(X,nada).
 
-mover(X,Y) :- not(bloqueado(X)), hastaArriba(Y), quitar(X,Y), sobre(X,Y).
+%se mueve X sobre Y y el sobre() dentro del parenteis es la referencia actualizada.
+mover(X,Y,(sobre(X,Y))) :- not(bloqueado(X)), hastaArriba(Y).
 
-quitar(X,Y) :- not(bloqueado(X)), sobre(X,Y).
-abajo(Y,X) :- sobre(X,Y).
+%quitarya esta bien implementado  quiarAux(estado anterior, estado despues de aplicar el predicado)
+quitarAux(sobre(X,Y),sobre(nada,Y)) :- not(bloqueado(X)), sobre(X,Y).
+
+%mover X sobre Y (estado de la pila donde estaba X, estado de la pila X sobre Y)
+ %quitar(sobre(X,Z),sobre(nada,Z)).
+
+%quitar(X,Y,N) :- not(bloqueado(X)), sobre(X,Y).
+%sobre(X,Y) :- discontiguous sobre(X,Y), mover(X,Y,(sobre(X,Y))).
+  %quitarAux(sobre(X,Y),sobre(nada,Y))
+
+%abajo(Y,X) :- sobre(X,Y).
+
+
+%will_be(put(grey, on, green), ([grey|Stack1], [green|Stack2]), (Stack1, [grey,green|Stack2])).
+%mover(put(X, on, Y), ([X|Stack1], [Y|Stack2]), (Stack1, [X,Y|Stack2])).
+
+%sobre(X,Y,[X,N]) :- sobre(nada,Y,N).
+
+%sobre(X,[nada],[X,nada]).
+%sobre(X,[Y|D],[X|E]) :- sobre(Y,D,E).
+
+%sobre(X,Y,[X|Y]).
+%sobre(X,)
+%sobre(X,Y,N) :- sobre(X,)
+%mover((X,Y), (sobre(X,XS,[X|XS]), sobre(Y,YS,[Y|YS])), (sobre(XS,XS), sobre(X,Y))).
+%will_be(Command, Before_state, After_state)
+
+
+/*lista([]):-!.
+lista([_|Y]):-lista(Y).
+
+elimina(X,[X|T],T).
+my_remove_element(_, [], []).
+
+my_remove_element(Y, [Y|Xs], Zs):-
+          my_remove_element(Y, Xs, Zs), !.
+
+my_remove_element(X, [Y|Xs], [Y|Zs]):-
+          my_remove_element(X, Xs, Zs).*/
